d8046554a890be2cbb8465605ee274606de3b1d6
Dixie Dean <d.w.sokolov26@gmail.com>
2023-09-15 14:18:16 +0300

    Added getUserAuthorities method

--- src/main/java/com/example/homeworkAuthorization/repository/UserRepository.java
+++ src/main/java/com/example/homeworkAuthorization/repository/UserRepository.java
@@ -3,11 +3,22 @@ package com.example.homeworkAuthorization.repository;
 import com.example.homeworkAuthorization.authorities.Authorities;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.*;
 
 @Repository
 public class UserRepository {
+
+    private final Map<String, String> users = new HashMap<>();
+
     public List<Authorities> getUserAuthorities(String user, String password) {
-        return null;
+        if (users.containsKey(user) && users.containsValue(password)) {
+            List<Authorities> authorities = new ArrayList<>();
+            authorities.add(Authorities.READ);
+            authorities.add(Authorities.WRITE);
+            authorities.add(Authorities.DELETE);
+            return authorities;
+        } else {
+            return Collections.emptyList();
+        }
     }
 }
