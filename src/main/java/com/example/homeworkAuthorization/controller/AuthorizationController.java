beb4da8c385d76515f8b54835986981e5f49de5e
Dixie Dean <d.w.sokolov26@gmail.com>
2023-09-15 16:15:36 +0300

    Added User and HMAResolver

--- src/main/java/com/example/homeworkAuthorization/controller/AuthorizationController.java
+++ src/main/java/com/example/homeworkAuthorization/controller/AuthorizationController.java
@@ -2,9 +2,12 @@ package com.example.homeworkAuthorization.controller;
 
 import com.example.homeworkAuthorization.authorities.Authorities;
 import com.example.homeworkAuthorization.service.AuthorizationService;
+import com.example.homeworkAuthorization.user.User;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.support.HandlerMethodArgumentResolverComposite;
 
 import java.util.List;
 
@@ -17,7 +20,8 @@ public class AuthorizationController {
     }
 
     @GetMapping("/authorize")
-    public List<Authorities> getAuthorities(@RequestParam("user") String user, @RequestParam("password") String password) {
-        return authorizationService.getAuthorities(user, password);
+    public List<Authorities> getAuthorities(@Valid User user) {
+
+        return authorizationService.getAuthorities(user);
     }
 }
